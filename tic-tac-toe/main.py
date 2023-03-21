469dbecb10642ed40ec02c0604feee2cca06abb5
antonella-giovannetti <127109361+antonella-giovannetti@users.noreply.github.com>
2023-03-21 11:38:35 +0100

    third commid - optimisation

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -2,196 +2,67 @@ from tkinter import *
 from tkinter import messagebox
 
 window = Tk()
-window.title('Tic tac toe')
+window.title('Jeu du morpion')
 
+# Variables globales
 clicked = True
 count = 0
+winner = False
+buttons = []
 
-# Remet à zéro les boutons
+# Désactive les boutons
 def disable_all_buttons():
-    b1.config(state=DISABLED) # désactive le bouton b1 en le mettant dans un état désactivé, l'utilisateur ne peut plus cliquer sur le bouton qui ne répondra plus aux événements de souris
-    b2.config(state=DISABLED)
-    b3.config(state=DISABLED)
-    b4.config(state=DISABLED)
-    b5.config(state=DISABLED)
-    b6.config(state=DISABLED)
-    b7.config(state=DISABLED)
-    b8.config(state=DISABLED)
-    b9.config(state=DISABLED)
+    for button in buttons:
+        button.config(state=DISABLED) # désactive le bouton en le mettant dans un état désactivé, l'utilisateur ne peut plus cliquer sur le bouton qui ne répondra plus aux événements de souris
 
-# Verifie si quelqu'un gagne
+# Verifie si O ou X gagne
 def checkifwon():
     global winner
-    winner = False
-    if b1["text"] == "X" and b2["text"] == "X" and b3["text"] == "X":
-        b1.config(bg="red")
-        b2.config(bg="red")
-        b3.config(bg="red")
-        winner = True
-        messagebox.showinfo("Tic tac toe", "X Wins !!")
-        disable_all_buttons()
-    elif b4["text"] == "X" and b5["text"] == "X" and b6["text"] == "X":
-        b4.config(bg="red")
-        b5.config(bg="red")
-        b6.config(bg="red")
-        winner = True
-        messagebox.showinfo("Tic tac toe", "X Wins !!")
-        disable_all_buttons()
-    elif b7["text"] == "X" and b8["text"] == "X" and b9["text"] == "X":
-        b4.config(bg="red")
-        b5.config(bg="red")
-        b6.config(bg="red")
-        winner = True
-        messagebox.showinfo("Tic tac toe", "X Wins !!")
-        disable_all_buttons()
-    elif b1["text"] == "X" and b4["text"] == "X" and b7["text"] == "X":
-        b1.config(bg="red")
-        b4.config(bg="red")
-        b7.config(bg="red")
-        winner = True
-        messagebox.showinfo("Tic tac toe", "X Wins !!")
-        disable_all_buttons()
-    elif b2["text"] == "X" and b5["text"] == "X" and b8["text"] == "X":
-        b2.config(bg="red")
-        b5.config(bg="red")
-        b8.config(bg="red")
-        winner = True
-        messagebox.showinfo("Tic tac toe", "X Wins !!")
-        disable_all_buttons()
-    elif b3["text"] == "X" and b6["text"] == "X" and b9["text"] == "X":
-        b3.config(bg="red")
-        b6.config(bg="red")
-        b9.config(bg="red")
-        winner = True
-        messagebox.showinfo("Tic tac toe", "X Wins !!")
-        disable_all_buttons()
-    elif b1["text"] == "X" and b5["text"] == "X" and b9["text"] == "X":
-        b1.config(bg="red")
-        b5.config(bg="red")
-        b9.config(bg="red")
-        winner = True
-        messagebox.showinfo("Tic tac toe", "X Wins !!")
-        disable_all_buttons()
-    elif b3["text"] == "X" and b5["text"] == "X" and b7["text"] == "X":
-        b3.config(bg="red")
-        b5.config(bg="red")
-        b7.config(bg="red")
-        winner = True
-        messagebox.showinfo("Tic tac toe", "O Wins !!")
-        disable_all_buttons()
-    elif b1["text"] == "O" and b2["text"] == "O" and b3["text"] == "O":
-        b1.config(bg="red")
-        b2.config(bg="red")
-        b3.config(bg="red")
-        winner = True
-        messagebox.showinfo("Tic tac toe", "O Wins !!")
-        disable_all_buttons()
-    elif b4["text"] == "O" and b5["text"] == "O" and b6["text"] == "O":
-        b4.config(bg="red")
-        b5.config(bg="red")
-        b6.config(bg="red")
-        winner = True
-        messagebox.showinfo("Tic tac toe", "O Wins !!")
-        disable_all_buttons()
-    elif b7["text"] == "O" and b8["text"] == "O" and b9["text"] == "O":
-        b4.config(bg="red")
-        b5.config(bg="red")
-        b6.config(bg="red")
-        winner = True
-        messagebox.showinfo("Tic tac toe", "O Wins !!")
-        disable_all_buttons()
-    elif b1["text"] == "O" and b4["text"] == "O" and b7["text"] == "O":
-        b1.config(bg="red")
-        b4.config(bg="red")
-        b7.config(bg="red")
-        winner = True
-        messagebox.showinfo("Tic tac toe", "O Wins !!")
-        disable_all_buttons()
-    elif b2["text"] == "O" and b5["text"] == "O" and b8["text"] == "O":
-        b2.config(bg="red")
-        b5.config(bg="red")
-        b8.config(bg="red")
-        winner = True
-        messagebox.showinfo("Tic tac toe", "O Wins !!")
-        disable_all_buttons()
-    elif b3["text"] == "O" and b6["text"] == "O" and b9["text"] == "O":
-        b3.config(bg="red")
-        b6.config(bg="red")
-        b9.config(bg="red")
-        winner = True
-        messagebox.showinfo("Tic tac toe", "O Wins !!")
-        disable_all_buttons()
-    elif b1["text"] == "O" and b5["text"] == "O" and b9["text"] == "O":
-        b1.config(bg="red")
-        b5.config(bg="red")
-        b9.config(bg="red")
-        winner = True
-        messagebox.showinfo("Tic tac toe", "O Wins !!")
-        disable_all_buttons()
-    elif b3["text"] == "O" and b5["text"] == "O" and b7["text"] == "O":
-        b3.config(bg="red")
-        b5.config(bg="red")
-        b7.config(bg="red")
-        winner = True
-        messagebox.showinfo("Tic tac toe", "O Wins !!")
-        disable_all_buttons()
-    if count == 9 and winner == False:
-        messagebox.showinfo("Tic tac toe", "No one Wins")
-        disable_all_buttons()
+    for a, b, c in [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7),(2, 5, 8),(0, 4, 8), (2, 4, 6)]: 
+        print(a, b, c)
+        if buttons[a]["text"] == buttons[b]["text"] == buttons[c]["text"] != "":
+            for button in (buttons[a], buttons[b], buttons[c]):
+                button.config(bg="blue")
+            winner = True
+            messagebox.showinfo("Jeu du morpion", f"Bravo, {buttons[a]['text']} a gagné !")
+            disable_all_buttons()
 
 # Boutons au click 
-def b_click(b):
-    global clicked, count # informe que les variables à l'exterieur sont les mêmes que définie à l'interieur de la fonction
-    if b["text"] == " " and clicked == True:
-        b["text"] = "X"
+def button_click(i):
+    global clicked, count, winner 
+    if buttons[i]["text"] == "" and clicked == True:
+        buttons[i]["text"] = "X"
         clicked = False
         count += 1
         checkifwon()
-    elif b["text"] == " " and clicked == False:
-        b["text"] = "O"
+    elif buttons[i]["text"] == "" and clicked == False:
+        buttons[i]["text"] = "O"
         clicked = True
         count += 1
         checkifwon()
-    else:
-        messagebox.showerror("Tic tac toe", "Hey! that box has already been selected \n  Pick another box")
-    
+    if count == 9 and winner == False:
+        messagebox.showinfo("Jeu du morpion", "Match nul")
+        disable_all_buttons()
 
 # Reinitialise le jeu de zéro 
 def reset(): 
-    global b1, b2, b3, b4, b5, b6, b7, b8, b9
-    global clicked, count
+    global buttons, clicked, count, winner
     clicked = True
     count = 0
-    # Boutons
-    b1 = Button(window, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b1))
-    b2 = Button(window, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b2))
-    b3 = Button(window, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b3))
-    b4 = Button(window, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b4))
-    b5 = Button(window, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b5))
-    b6 = Button(window, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b6))
-    b7 = Button(window, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b7))
-    b8 = Button(window, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b8))
-    b9 = Button(window, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b9))
+    winner = False
+    for button in buttons:
+        button.config(text="", bg="SystemButtonFace", state=NORMAL)
 
-    # Emplacement des boutons sur une grille
-    b1.grid(row=0, column=0)
-    b2.grid(row=0, column=1)
-    b3.grid(row=0, column=2)
-
-    b4.grid(row=1, column=0)
-    b5.grid(row=1, column=1)
-    b6.grid(row=1, column=2)
-
-    b7.grid(row=2, column=0)
-    b8.grid(row=2, column=1)
-    b9.grid(row=2, column=2)
+# Actions
+for i in range(9):
+    button = Button(window, text="", font=("Helvetica", 20), height=3, width=6, command=lambda i=i: button_click(i))
+    button.grid(row=i // 3, column=i % 3, sticky=NSEW) # sticky NSEW fait en sorte que le bouton occupe tout l'espace disponible dans sa cellule
+    buttons.append(button)
+print(buttons)
 
 # Menu
 my_menu = Menu(window) # Création de l'objet de menu 
 window.config(menu=my_menu) # Affiche le menu crée precédement en utilisant la méthode config et en passant le param menu qui prend l'objet de menu crée
-
-my_menu.add_command(label="Reset Game", command=reset)  # Ajoute une option au menu principal "Reset game" et la fonction reset 
-
-reset()
+my_menu.add_command(label="Relancer le jeu", command=reset)  # Ajoute une option au menu principal "Reset game" et la fonction reset 
 
 window.mainloop()
